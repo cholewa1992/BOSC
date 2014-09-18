0a5f6da3182b9bd87a6b54646ec0c71ea9ae9a98
Jacob B. Cholewa <jbec@itu.dk>
2014-09-18 13:17:34 +0200

    Added files:

--- oo1/bosh.c
+++ oo1/bosh.c
@@ -1,8 +1,6 @@
 /* 
-
    bosh.c : BOSC shell 
-
- */
+*/
 
 #include <stdio.h>
 #include <string.h>
@@ -20,45 +18,39 @@
 /* --- use the /proc filesystem to obtain the hostname --- */
 char *gethostname(char *hostname)
 {
-  hostname = "";
-  return hostname;
+	hostname = "cool";
+	return hostname;
 }
 
 /* --- execute a shell command --- */
 int executeshellcmd (Shellcmd *shellcmd)
 {
-  printshellcmd(shellcmd);
-
-  return 0;
+	printshellcmd(shellcmd);
+ 	return 0;
 }
 
-/* --- main loop of the simple shell --- */
-int main(int argc, char* argv[]) {
-
-  /* initialize the shell */
-  char *cmdline;
-  char hostname[HOSTNAMEMAX];
-  int terminate = 0;
-  Shellcmd shellcmd;
+	/* --- main loop of the simple shell --- */
+	/* initialize the shell */
+	char *cmdline;
+	char hostname[HOSTNAMEMAX];
+	int terminate = 0;
+	Shellcmd shellcmd;
   
-  if (gethostname(hostname)) {
-
-    /* parse commands until exit or ctrl-c */
-    while (!terminate) {
-      printf("%s", hostname);
-      if (cmdline = readline(":# ")) {
-	if(*cmdline) {
-	  add_history(cmdline);
-	  if (parsecommand(cmdline, &shellcmd)) {
-	    terminate = executeshellcmd(&shellcmd);
-	  }
-	}
-	free(cmdline);
-      } else terminate = 1;
-    }
-    printf("Exiting bosh.\n");
-  }    
-    
-  return EXIT_SUCCESS;
+  	if (gethostname(hostname)) {
+    		/* parse commands until exit or ctrl-c */
+    		while (!terminate) {
+      			printf("%s", hostname);
+      			if (cmdline = readline(":# ")) {
+				if(*cmdline) {
+	  				add_history(cmdline);
+	  				if (parsecommand(cmdline, &shellcmd)) {
+	    					terminate = executeshellcmd(&shellcmd);
+	 				}
+				}
+				free(cmdline);
+    			 } else terminate = 1;
+   		 }
+   		 printf("Exiting bosh.\n");
+  	}    
+  	return EXIT_SUCCESS;
 }
-
